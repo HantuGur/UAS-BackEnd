72e8ba2b83f108ed5275a12966b2b60ec30e9d1f
csrhlty <aurel.lieha@gmail.com>
2026-06-02 20:11:49 +0700

    refactor: mengganti kolom phone ke email opsional di tabel customer

--- database/migrations/2026_05_27_083435_create_customers_table.php
+++ database/migrations/2026_05_27_083435_create_customers_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamps();
         });
     }
