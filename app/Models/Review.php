0d1367ef6e40a09d4ed994eac867f08feadcef25
davvinfusta@gmail.com <davvinfusta@gmail.com>
2026-06-04 23:37:11 +0700

    cek error

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
-{
-    //
+class Review extends Model {
+    use HasFactory;
+    protected $fillable = ['customer_id', 'menu_id', 'rating', 'comment'];
+
+    public function customer() { return $this->belongsTo(Customer::class); }
+    public function menu() { return $this->belongsTo(Menu::class); }
 }
