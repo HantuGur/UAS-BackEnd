0283fc3b5790c661fe33a74e93eb095260df3758
csrhlty <aurel.lieha@gmail.com>
2026-06-08 22:43:32 +0700

    feature: mengupdate StoreTableRequest dan StorePaymentRequest dengan validasi input

--- app/Http/Requests/StorePaymentRequest.php
+++ app/Http/Requests/StorePaymentRequest.php
@@ -7,11 +7,11 @@ class StorePaymentRequest extends FormRequest
     public function authorize(): bool { return true; }
     public function rules(): array {
         return [
-            'order_id' => 'required|exists:orders,id',
-            'payment_method' => 'required|string|in:cash,qris',
-            'amount' => 'required|integer|min:0',
-            'cash_received' => 'nullable|integer|min:0',
-            'promo_id' => 'nullable|exists:promos,id'
+            'order_id'       => 'required|exists:orders,id',
+            'payment_method' => 'required|in:cash,qris',
+            'amount'         => 'required|integer|min:0',
+            'cash_received'  => 'nullable|integer|min:0',
+            'promo_id'       => 'nullable|exists:promos,id',
         ];
     }
 }
