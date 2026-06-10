e4f0b6d91ef71bc6c83944dd53b9d2a485dba6fb
Olivian Cerlindo <oliviancerlindo@gmail.com>
2026-06-10 13:03:32 +0700

    feature(middleware): menambahkan CustomerMiddleware untuk proteksi rute halaman belanja pelanggan

---
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!session()->has('customer_id')) {
+            return redirect()->route('customer.login')->with('error', 'Silakan login sebagai pelanggan terlebih dahulu.');
+        }
+        return $next($request);
+    }
+}
