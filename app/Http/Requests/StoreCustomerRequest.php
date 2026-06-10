b55bf5b25bb9497535682b024ac3f15a588367c8
davvinfusta@gmail.com <davvinfusta@gmail.com>
2026-06-10 14:17:48 +0700

    Update yang error di tampilan webnya

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -7,7 +7,7 @@ class StoreCustomerRequest extends FormRequest {
     public function rules(): array {
         return [
             'name'  => 'required|string|max:255',
-            'phone' => 'nullable|string|max:20',
+            'email' => 'required|email|max:255',
         ];
     }
 }
